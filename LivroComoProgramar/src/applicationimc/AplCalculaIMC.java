14d8021cfbc0ba8eaf52e4066431a41bda708f89
Jeferson Luis <jgjefersonluis@gmail.com>
2020-03-22 03:21:01 -0300

    Capitulo 2 livro Como Programar eidcao8 e IMC

---
+++ LivroComoProgramar/src/applicationimc/AplCalculaIMC.java
@@ -0,0 +1,10 @@
+package applicationimc;
+
+
+public class AplCalculaIMC {
+
+	public static void main(String[] args) {
+		new GuiPrincipal().setVisible(true);
+	}
+
+}
---
+++ LivroComoProgramar/src/capitulo2/Welcome3.java
@@ -0,0 +1,8 @@
+package capitulo2;
+
+public class Welcome3 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome\n to \n Java\n Programming!");
+	}
+}
