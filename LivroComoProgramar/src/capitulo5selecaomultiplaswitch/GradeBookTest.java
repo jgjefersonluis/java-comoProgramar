1743bdf4e2dd62dd66ecc4eae7d0b63fe333729d
Jeferson Luis <jgjefersonluis@gmail.com>
2020-03-27 10:07:37 -0300

    capitulo 5 instruções de repetição

---
+++ LivroComoProgramar/src/capitulo5selecaomultiplaswitch/GradeBookTest.java
@@ -0,0 +1,16 @@
+package capitulo5selecaomultiplaswitch;
+
+public class GradeBookTest {
+
+	public static void main(String[] args) {
+
+		GradeBook myGradeBook = new GradeBook(
+				"CS101 Introduction to Java Programming");
+
+		myGradeBook.displayMessage();
+		myGradeBook.inputGrades();
+		myGradeBook.displayGradeReport();
+
+	}
+
+}
